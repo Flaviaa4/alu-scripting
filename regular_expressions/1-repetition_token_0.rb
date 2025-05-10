a76d1f0201eac43ee261a36210301b348b67a716
Flaviaa4 <f.benihirwe@alustudent.com>
2025-05-10 10:10:26 -0700

    Updated content

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-regex = /Repetition Token #\d+/
-
-if ARGV.length != 1
-  puts "Usage: ruby 1-repetition_token_0.rb 'Your string here'"
-  exit
-end
-
-input_string = ARGV[0]
-
-if input_string.match?(regex)
-  puts "Match found: #{input_string}"
-else
-  puts "No match found."
-end
-
+puts ARGV[0].scan(/hbt{2,5}n/).join
